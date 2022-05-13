d133ebc3fdaf2216e579829d13ba5a050d390f4c
fatherice <fatherice@gmail.com>
2022-05-13 11:59:46 +0300

    Result Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getName());
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getName());
+
+        System.out.println();
+        System.out.println(bean.equals(bean1));
+        System.out.println(beanCat.equals(beanCat1));
     }
 }
